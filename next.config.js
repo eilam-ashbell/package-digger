972a3026af4b007945eefe7c6ae356f28649152a
eilam-ashbell <eashbell@gmail.com>
2023-05-20 20:26:51 +0300

    init

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpack: (config) => {
+        // this will override the experiments
+        config.experiments = { ...config.experiments, topLevelAwait: true };
+        // this will just update topLevelAwait property of config.experiments
+        // config.experiments.topLevelAwait = true 
+        return config;
+      },
+      experimental: {
+        appDir: true,
+        serverComponentsExternalPackages: ["mongoose"],
+      },
+}
 
 module.exports = nextConfig
