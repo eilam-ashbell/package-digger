8a3f3af0a24215c10f72735d9e7970c4c3e13b25
eilam-ashbell <eashbell@gmail.com>
2023-06-05 18:55:48 +0300

    git getRepo & getUser

---
+++ models/git/license-model.ts
@@ -0,0 +1,8 @@
+
+export default class LicenseModel {
+    "key": string;
+    "name": string;
+    "spdx_id": string;
+    "url": string;
+    "node_id": string;
+};
---
+++ utils/git.ts
@@ -0,0 +1,23 @@
+import RepoModel from "@/models/git/repo-model";
+import UserModel from "@/models/git/user-model";
+import axios from "axios";
+
+
+async function getRepo(owner: string, repo: string): Promise<RepoModel> {
+    const { data } = await axios.get<RepoModel>(
+        `https://api.github.com/repos/${owner}/${repo}`
+    );
+    return data;
+}
+
+async function getUser(username: string) {
+    const { data } = await axios.get<UserModel>(
+        `https://api.github.com/users/${username}`
+    );
+    return data;
+}
+
+export default {
+    getRepo,
+    getUser,
+}
